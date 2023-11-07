d27ed937a9dfbd255adddae360996987d3a6a794
lclhugo <h.laclau@human-immobilier.fr>
2023-11-07 09:35:22 +0100

    navbar rework

--- components/layout/TheNavbar.vue
+++ components/layout/TheNavbar.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <header>
     <div id="app" class="font-sans antialiased">
       <nav class="flex flex-wrap items-center justify-between p-6 bg-teal">
@@ -50,8 +50,8 @@
       </nav>
     </div>
   </header>
-</template>
-
+</template> -->
+<!-- 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
 
@@ -67,4 +67,97 @@ export default defineComponent({
     };
   },
 });
+</script> -->
+
+<template>
+  <div class="navbar bg-base-100">
+    <div class="navbar-start">
+      <div class="dropdown">
+        <label tabindex="0" class="btn btn-ghost lg:hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="w-5 h-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h8m-8 6h16"
+            />
+          </svg>
+        </label>
+        <ul
+          tabindex="0"
+          class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+        >
+          <li>
+            <a>Parent</a>
+            <ul class="p-2">
+              <li><a>Submenu 1</a></li>
+              <li><a>Submenu 2</a></li>
+            </ul>
+          </li>
+          <li>
+            <a>Parent</a>
+            <ul class="p-2">
+              <li><a>Submenu 1</a></li>
+              <li><a>Submenu 2</a></li>
+            </ul>
+          </li>
+          <li>
+            <a>Parent</a>
+            <ul class="p-2">
+              <li><a>Submenu 1</a></li>
+              <li><a>Submenu 2</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <NuxtLink to="/" class="text-xl font-semibold tracking-tight normal-case btn btn-ghost">
+        AnimeList
+      </NuxtLink>
+    </div>
+    <div class="hidden navbar-center lg:flex">
+      <ul class="gap-8 px-1 menu menu-horizontal">
+        <li tabindex="0">
+          <details>
+            <summary>Anime</summary>
+            <ul class="p-2">
+              <li><a>Submenu 1</a></li>
+              <li><a>Submenu 2</a></li>
+            </ul>
+          </details>
+        </li>
+
+        <li tabindex="0">
+          <details>
+            <summary>Manga</summary>
+            <ul class="p-2">
+              <li><a>Submenu 1</a></li>
+              <li><a>Submenu 2</a></li>
+            </ul>
+          </details>
+        </li>
+        <li tabindex="0">
+          <details>
+            <summary>List</summary>
+            <ul class="p-2">
+              <li><a>Submenu 1</a></li>
+              <li><a>Submenu 2</a></li>
+            </ul>
+          </details>
+        </li>
+      </ul>
+    </div>
+    <div class="navbar-end">
+      <ThemeSwitcher />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import type { ThemeSwitcher } from '#build/components';
 </script>
